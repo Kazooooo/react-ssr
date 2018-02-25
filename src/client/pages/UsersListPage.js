f3f43deb009735d3a26051b453bcd28444bb7a5e
Kazooooo <cest93@gmail.com>
2018-02-25 20:17:47 +0900

    react-helmetでtitleタグとmetaタグを追加、SSRでも表示されるようにhtmlテンプレートにも追加

--- src/client/pages/UsersListPage.js
+++ src/client/pages/UsersListPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { fetchUsers } from "../actions";
+import { Helmet } from "react-helmet";
 
 class UserListPage extends React.Component {
   componentDidMount() {
@@ -16,6 +17,10 @@ class UserListPage extends React.Component {
   render() {
     return (
       <div>
+        <Helmet>
+          <title>Users App</title>
+          <meta property="og:title" content="Users App" />
+        </Helmet>
         Here's a big list of users:
         <ul>{this.renderUsers()}</ul>
       </div>
