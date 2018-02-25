ff6e3ec746c7a41259e79224ed843b2992f93cef
Kazooooo <cest93@gmail.com>
2018-02-25 15:58:14 +0900

    Routesで全ページファイルからloadDataをimportすると名前が競合してしまうので、exportをオブジェクト化する

--- src/client/pages/UsersListPage.js
+++ src/client/pages/UsersListPage.js
@@ -31,5 +31,7 @@ function loadData(store) {
   return store.dispatch(fetchUsers());
 }
 
-export { loadData };
-export default connect(mapStateToProps, { fetchUsers })(UserListPage);
+export default {
+  loadData,
+  component: connect(mapStateToProps, { fetchUsers })(UserListPage),
+};
